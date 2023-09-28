5a252135ec3d6a1303828ad66eb645d1054238b1
Churros <klebersonluiz5@gmail.com>
2023-09-27 23:42:41 -0300

    Finish

---
+++ life-cycle/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'life-cycle';
+  isAliveCheckSample:boolean = true
+
+  disposeCheckSample():void{
+    this.isAliveCheckSample = false
+  }
+  addCheckSample():void{
+    this.isAliveCheckSample = true
+  }
+
+}
